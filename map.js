e6f7de4784e4b03cb5995da5dcc821bf4d5a4429
Jay Wong <xiao.hk1997@gmail.com>
2019-01-27 23:38:56 -0600

    Fix JS formating, add a slider

--- map.js
+++ map.js
@@ -5,20 +5,28 @@
  *
  */
 
+ /* global d3 */
+
 // Set up the size of SVG element
-var width = 960,
+let width = 960,
     height = 600;
 
-var svg = d3.select("svg")
+let sliderWidth = 600;
+
+// Global values for semesters
+let semesters = [2017.5, 2018, 2018.5];
+let formatSemester = d => Math.floor(d) === d ? `${d} Spring` : `${Math.floor(d)} Fall`;
+
+let svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height);
 
 // Make projection for the china map
-var projection = d3.geoMercator()
+let projection = d3.geoMercator()
     .center([116,39])
     .scale(620);
   
-var path = d3.geoPath()
+let path = d3.geoPath()
     .projection(projection);
 
 // Bind GeoJSON data to D3
@@ -39,33 +47,33 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
 
     // Preparing statistics for making more interesting aesthetics
     // Count cities form the reward table
-    var cities = {};
-    for (var entry of reward){
-        city = entry["city"];
+    let cities = {};
+    for (let entry of reward){
+        let city = entry.city;
         if (city in cities){
-            cities[city]["count"] ++;
+            cities[city].count ++;
         } else {
             cities[city] = {"count": 1};
         }
     }
 
     // Get the coordinate and chinese name of each city in the reward table
-    for (var city in cities){
-        cities[city]["hanzi"] = coord[city]["hanzi"];
-        cities[city]["coord"] = coord[city]["coord"].reverse();
+    for (let city in cities){
+        cities[city].hanzi = coord[city].hanzi;
+        cities[city].coord = coord[city].coord.reverse();
     }
 
     // Change the city dictionary to array, so we can use data() more easily
     // Also sort by the count, so smaller circle show on top
-    var city_array = d3.entries(cities).sort(function(a, b) {
-        return b["value"].count - a["value"].count;
+    let city_array = d3.entries(cities).sort(function(a, b) {
+        return b.value.count - a.value.count;
     });
 
     // Create a scale for circle area
-    var areaScale = d3.scaleSqrt()
+    let areaScale = d3.scaleSqrt()
         .domain([
             0,
-            d3.max(city_array, function(d) { return d["value"].count; })
+            d3.max(city_array, function(d) { return d.value.count; })
         ])
         .range([0, 30]);
 
@@ -74,9 +82,9 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
         .data(city_array)
         .enter()
         .append("circle")
-        .attr("r", function(d) { return areaScale(d["value"].count); })
-        .attr("cx", function(d) { return projection(d["value"].coord)[0]; })
-        .attr("cy", function(d) { return projection(d["value"].coord)[1]; })
+        .attr("r", function(d) { return areaScale(d.value.count); })
+        .attr("cx", function(d) { return projection(d.value.coord)[0]; })
+        .attr("cy", function(d) { return projection(d.value.coord)[1]; })
         .style("fill-opacity", "0.2")
         .style("fill", "red")
         .style("stroke", "white")
@@ -88,15 +96,15 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
                 .style("stroke-width", "1px");
             showInfoText(d);
         })
-        .on("mouseout",function(d){
+        .on("mouseout",function(){
             d3.select(this)
                 .style("stroke", "white")
                 .style("stroke-width", ".3px");
-            hideInfoText(d);
+            hideInfoText();
         });
     
     // Add a legend layer
-    var legend = svg.append("g")
+    let legend = svg.append("g")
         .attr("transform", "translate(" + (width - 250) + "," +
               (height - 100) + ")")
         .selectAll("g")
@@ -121,19 +129,19 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
         .style("fill", "#606060");
 
     // Add a title layer
-    svg.append("text")
+    let title = svg.append("text")
         .attr("x", width / 2)    
         .attr("y", height - 70)
         .attr("text-anchor", "middle")
         .style("font", "16px serif")
         .style("fill", "#606060")
-        .text("Chinese Students on UW-Madison Dean's List (2018 Spring)");
+        .text(`Chinese Students on UW-Madison Dean's List (${formatSemester(d3.max(semesters))})`);
 
     // Add info text box
-    var text_pos = [width - 300, height - 300];
-    var info_texts = [];
+    let text_pos = [width - 300, height - 300];
+    let info_texts = [];
 
-    for (var i = 0; i < 3; i ++){
+    for (let i = 0; i < 3; i ++){
         info_texts.push(
             svg.append("text")
                 .attr("x", text_pos[0])
@@ -144,6 +152,24 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
         );
     }
 
+    // Add a slider to alter semesters
+    let makeSlider = d3.sliderHorizontal()
+        .min(d3.min(semesters))
+        .max(d3.max(semesters))
+        .step(0.5)
+        .width(sliderWidth)
+        .tickFormat(formatSemester)
+        .ticks(3)
+        .default(d3.max(semesters))
+        .displayValue(false)
+        .on('onchange', val => {
+            title.text(`Chinese Students on UW-Madison Dean's List (${formatSemester(val)})`);
+        });
+
+    svg.append("g")
+        .attr('transform', 'translate(100, 550)')
+        .call(makeSlider);
+
     function showInfoText(d){
         /*
          * When mouse over any circles, display details in the info text
@@ -153,29 +179,29 @@ d3.csv("./data/award_table_2018s.csv", function(error, reward){
          */
         
         // Load specific text for each line
-        info_texts[0].text("City: " + d["key"][0].toUpperCase() + 
-            d["key"].substring(1) + " (" + d["value"]["hanzi"] + ")");
+        info_texts[0].text("City: " + d.key[0].toUpperCase() + 
+            d.key.substring(1) + " (" + d.value.hanzi + ")");
 
-        info_texts[1].text("Coord: " + d["value"].coord[1] + "° N, " +
-            d["value"].coord[0] + "° E");
+        info_texts[1].text("Coord: " + d.value.coord[1] + "° N, " +
+            d.value.coord[0] + "° E");
 
-        info_texts[2].text("Student Counts: " + d["value"].count);
+        info_texts[2].text("Student Counts: " + d.value.count);
 
-        for (var i = 0; i < 3; i ++){
+        for (let i = 0; i < 3; i ++){
             info_texts[i].attr("class", "info_text_show");
         }
     }
 
-    function hideInfoText(d){
+    function hideInfoText(){
         /*
          * When mouse out any circles, hide details in the info text
          *
          * Args:
          *      d: one key value pair object from data()
          */
-        for (var i = 0; i < 3; i ++){
+        for (let i = 0; i < 3; i ++){
             info_texts[i].attr("class", "info_text_hidden");
         }
     }
 
-})})});
+});});});
